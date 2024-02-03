c0c045e554500f46d23c732297268916b12c1c13
VinVlad <93072524+VinVlad@users.noreply.github.com>
2024-02-03 20:25:38 +1000

    Add file

---
+++ utils.go
@@ -0,0 +1,21 @@
+package utils
+
+func Contains(a []string, x string) bool {
+
+	for _, n := range a {
+		if n == x {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt(a []int, x int) bool {
+
+	for _, n := range a {
+		if n == x {
+			return true
+		}
+	}
+	return false
+}
